c472c7148c2ed54b1e9fb7aa9f5b704ed3b467af
AnthonyScott <anthony.scott1005@gmail.com>
2023-06-18 23:42:48 -0400

    Add methods for retrieving one book and all books

--- Repositories/InMemBookstoreRepository.cs
+++ Repositories/InMemBookstoreRepository.cs
@@ -1,6 +1,7 @@
 using Bookstore.Models;
 
 
+
 namespace Bookstore.Repositores;
 
 public class InMemBookstoreRepository
@@ -10,12 +11,17 @@ public class InMemBookstoreRepository
         new Book { Id = Guid.NewGuid(), Price = 9.99, Title = "Great Expectations", DateCreated = DateTimeOffset.UtcNow },
         new Book { Id = Guid.NewGuid(), Price = 99.99, Title = "Finch-Collector's Death", DateCreated = DateTimeOffset.UtcNow },
         new Book { Id = Guid.NewGuid(), Price = 5.99, Title = "Auth, the Great Nightmare", DateCreated = DateTimeOffset.UtcNow },
-        new Book { Id = Guid.NewGuid(), Price = 1.99, Title = "Job Searching was Hell until I found this trick!", DateCreated = DateTimeOffset.UtcNow }
+        new Book { Id = Guid.NewGuid(), Price = 150.99, Title = "Job Searching was Hell until I found this trick!", DateCreated = DateTimeOffset.UtcNow }
     };
 
     public IEnumerable<Book> GetBooks()
     {
         return books;
+    }
+
+    public Book GetBook(Guid id)
+    {
+        return books.Where(book => book.Id == id).SingleOrDefault();
     }
 
 }
