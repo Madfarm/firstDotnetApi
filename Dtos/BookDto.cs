2d264b46a1e95d921d79bff498d07cb3b8c2b2a1
AnthonyScott <anthony.scott1005@gmail.com>
2023-06-21 12:51:37 -0400

    Update DTO to only return the price and title of the book

--- Dtos/BookDto.cs
+++ Dtos/BookDto.cs
@@ -1,7 +1,5 @@
 public record BookDto
 {
-    public Guid Id { get; init; }
     public double Price { get; init; }
     public string Title { get; init; } = null!;
-    public DateTimeOffset DateCreated { get; init; }
 }
